7f497c790dd3a5e49d3d2379b5b0470a51d90a8e
timo1227 <timestrada@vm1.c.cs-288-linux.internal>
2022-12-09 01:49:22 +0000

    HW2 Working

---
+++ HW10/Problem2/HW10_Problem2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+
+typedef struct
+{
+    int num_threads;
+    long *id;
+    long n;
+    long double *pi_approx;
+} thread_data;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+int counter = 0;
+
+void CalcPiAprox(void *threadarg)
+{
+    // Lock the mutex
+    pthread_mutex_lock(&lock);
+    // Get the thread number
+    long thread_num = ((thread_data *)threadarg)->id[counter];
+    counter++;
+    // Get the number of terms
+    long n = ((thread_data *)threadarg)->n;
+    // Get the number of threads
+    int num_threads = ((thread_data *)threadarg)->num_threads;
+    // Get the array of approximations
+    long double *pi_approx = ((thread_data *)threadarg)->pi_approx;
+
+    // Calculate the number of terms for each thread
+    long double x;
+    long double partial_sum = 0.0;
+    for (int i = n / num_threads * thread_num + 1; i <= n / num_threads * (thread_num + 1); i++)
+    {
+        // Calculate the approximation
+        x = ((double)i - 0.5) / n;
+        partial_sum += 4.0 / (1.0 + x * x);
+    }
+    pi_approx[thread_num] = partial_sum;
+    // Unlock the mutex
+    pthread_mutex_unlock(&lock);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char const *argv[])
+{
+    long double PI = 3.14159265358979323846264338327950288419716939937510;
+    // Create a thread_data struct
+    thread_data *data = malloc(sizeof(thread_data));
+    // Get the number of threads from the command line
+    data->num_threads = atoi(argv[1]);
+    // Get the number of terms from user input
+    printf("Enter the number of terms: ");
+    scanf("%ld", &data->n);
+    // Create an array to store the approximations
+    data->pi_approx = malloc(data->num_threads * sizeof(long));
+    // Create an array of threads
+    pthread_t *threads = malloc(data->num_threads * sizeof(pthread_t));
+    // Create array of thread numbers
+    data->id = malloc(data->num_threads * sizeof(long));
+    // Create the threads
+    for (int i = 0; i < data->num_threads; i++)
+    {
+        data->id[i] = i;
+        pthread_create(&threads[i], NULL, (void *)CalcPiAprox, (void *)data);
+    }
+    // Wait for the threads to finish
+    for (int i = 0; i < data->num_threads; i++)
+    {
+        printf("Waiting for thread %d to finish\n", i);
+        pthread_join(threads[i], NULL);
+        printf("Thread %d finished\n", i);
+    }
+    // Calculate the final approximation
+    long double pi_approx = 0;
+    for (int i = 0; i < data->num_threads; i++)
+    {
+        pi_approx += data->pi_approx[i];
+    }
+    long double pi = pi_approx / data->n;
+    // Print the final approximation
+    printf("Pi is oapproximatlery %.22Lf, Error is %.22LF\n", pi, fabsl(pi - PI));
+    return 0;
+}
